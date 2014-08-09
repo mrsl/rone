7ca5cdc354450fc1887ced7ec428b1e7d9bd2b79
samtormey <samueltormey2@gmail.com>
2014-08-09 11:07:44 -0500

    metadata

--- software/projects/BilliardBots/bin_rone_v12/src/subdir.mk
+++ software/projects/BilliardBots/bin_rone_v12/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/irRangeTest.c 
+../src/irBilliardsTest\ .c 
 
 OBJS += \
-./src/irRangeTest.o 
+./src/irBilliardsTest\ .o 
 
 C_DEPS += \
-./src/irRangeTest.d 
+./src/irBilliardsTest\ .d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.c
+src/irBilliardsTest\ .o: ../src/irBilliardsTest\ .c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross ARM C Compiler'
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g3 -I"C:\Users\Sam\Documents\rone\software\system\driverlib\inc" -I"C:\Users\Sam\Documents\rone\software\system\FreeRTOS\inc" -I"C:\Users\Sam\Documents\rone\software\system\roneos\inc" -I"C:\Users\Sam\Documents\rone\software\system\roneLib\inc" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g3 -I"C:\Users\Sam\Documents\rone\software\system\driverlib\inc" -I"C:\Users\Sam\Documents\rone\software\system\FreeRTOS\inc" -I"C:\Users\Sam\Documents\rone\software\system\roneos\inc" -I"C:\Users\Sam\Documents\rone\software\system\roneLib\inc" -std=gnu11 -MMD -MP -MF"src/irBilliardsTest .d" -MT"src/irBilliardsTest\ .d" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
