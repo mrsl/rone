482c145f68871890815a3255038cc6e645c0374b
zkingston <kingston.zak@gmail.com>
2015-05-13 16:51:00 -0500

    Updated version of MSP code

--- software/msp430/BottomBoard/src/VersionNumber.h
+++ software/msp430/BottomBoard/src/VersionNumber.h
@@ -1,7 +1,7 @@
 #ifndef VERSIONNUMBER_H_
 #define VERSIONNUMBER_H_
 
-/* 
+/*
  * Hardware version numbers need to fit in 2 bits. Start at 1 just because. Need a new version
  * number for each build configuration.
  */
@@ -13,7 +13,7 @@
 	#define HARDWARE_VERSION_NUMBER    2
 #endif
 
-#define	PROGRAM_VERSION_NUMBER		22
+#define	PROGRAM_VERSION_NUMBER		23
 
 /* Bitpack the version ID byte */
 #define FULL_ID_VERSION_NUMBER   (((HARDWARE_VERSION_NUMBER & 0x3) << 6) | (PROGRAM_VERSION_NUMBER & 0x3F))
